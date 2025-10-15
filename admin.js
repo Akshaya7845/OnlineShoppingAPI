60b8c0ff3cba8d01d16f32ef6155a7c7da2dc662
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-15 11:22:37 +0530

    cart ordr admin and user changes applied

--- admin.js
+++ admin.js
@@ -11,8 +11,8 @@ function getEmail(){ r
   return sessionStorage.getItem("user_email"); 
 }
 function headers(){ const t=getToken(); return t?{ 
-  Authorization:`Bearer ${"ya29.a0AQQ_BDTEA-p5-0zoWg4eKVp5k1_cfRVnKMMcRWXLjfYF1TGx6BZA3gGcU7IehjMtYoSFD01MbgO3uZ6CtUp8yJhm21WVAa67Hi-BZNsucJz3pftfR3lXrCHm2A_IFv9mNn2UEaRweul6YrWKzVMbFKaebblbtPwnv5jXiFrR_qssg5HQriJWdnRNkofos2cA__cAAFcaCgYKAQgSARESFQHGX2Mi912baazITZYzsiQzxN-2Vg0206"}` } :
- {}; }
+  Authorization:`Bearer ${//"ya29.a0AQQ_BDTEA-p5-0zoWg4eKVp5k1_cfRVnKMMcRWXLjfYF1TGx6BZA3gGcU7IehjMtYoSFD01MbgO3uZ6CtUp8yJhm21WVAa67Hi-BZNsucJz3pftfR3lXrCHm2A_IFv9mNn2UEaRweul6YrWKzVMbFKaebblbtPwnv5jXiFrR_qssg5HQriJWdnRNkofos2cA__cAAFcaCgYKAQgSARESFQHGX2Mi912baazITZYzsiQzxN-2Vg0206"}` } :
+ {}//; }
 function fsURL(col){ 
   return `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/${encodeURIComponent(col)}`; 
 }
